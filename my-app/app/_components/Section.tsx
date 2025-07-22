65aa63eee344f68156edc60cfb7f8fb6a99cdc3b
Peuraud-Pierre <peuraudpierre9@gmail.com>
2025-07-22 12:45:16 +0200

    Nettoyage complet de la branche gh-pages

--- my-app/app/_components/Section.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from "react";
-import { cn } from "@/lib/utils";
-
-export const Section = (props: PropsWithChildren<{ classname?: string }>) => {
-  return (
-    <section className={cn("max-w-4xl px-4 m-auto", props.classname)}>{props.children}</section>
-  );
-};
